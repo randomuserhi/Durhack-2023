0bd319d2dab9a8cc480d01fb62e5ebbaa9e765c2
Anivarth <47830353+Lagombi01@users.noreply.github.com>
2023-11-04 14:33:45 +0000

    Making Server
    Server backend created

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+//https://stackoverflow.com/questions/57009371/access-to-xmlhttprequest-at-from-origin-localhost3000-has-been-blocked
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static("static"));
+app.use(express.json());
+
+/*
+app.get("/", function(req, resp) {
+    resp.sendFile(`${__dirname}/client-page/index.html`);
+});
+
+app.use(express.static("client-page"));
+app.use(express.json());
+
+app.get("/client-page/", function(req, resp) {
+    resp.sendFile(`${__dirname}/static/main.html`);
+});
+*/
+
+
+/*
+let data = [];
+
+app.post("/report", function(req, resp){
+    console.log("report called")
+    try{
+        data.push(req.body);
+        resp.send(JSON.stringify("Report successfully sent."));
+    } catch(e) {
+        console.log(e)
+        resp.send(JSON.stringify("Error submitting report, please try again later."));
+    }    
+});
+
+app.get("/refresh", function(req, resp){
+    
+    try{
+        resp.send(JSON.stringify(data));
+        data = [];
+    } catch(e) {
+        console.log(e);
+        resp.send(JSON.stringify("Error sending heatmap data. please try again later."));
+    }
+});
+*/
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+const port = 7676;
+
+app.listen(port, function() {
+    console.log(`Epic Poggers Analysis Tool listening on Port ${port}!`);
+});
